cbf99de8228b2af3f977ad02f69cbe17d261ba9c
ashiyana10 <71968071+ashiyana10@users.noreply.github.com>
2024-09-25 11:22:30 +0530

    Update Program.cs

--- Assignment 3 c#/Program.cs
+++ Assignment 3 c#/Program.cs
@@ -5,19 +5,19 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Dictionary<int,string> PrimeMinisterData = new Dictionary<int,string>();
-        PrimeMinisterData.Add(1998, "Atal Bihari Vajpayee");
-        PrimeMinisterData.Add(2014, "Narendra Modi");
-        PrimeMinisterData.Add(2004, "Manmohan Singh");
+        Dictionary<int,string> primeMinisterData = new Dictionary<int,string>();
+        primeMinisterData.Add(1998, "Atal Bihari Vajpayee");
+        primeMinisterData.Add(2014, "Narendra Modi");
+        primeMinisterData.Add(2004, "Manmohan Singh");
 
         // Find the Prime minister of 2004.
-        Console.WriteLine($"Prime minister of 2004: {PrimeMinisterData[2004]}");
+        Console.WriteLine($"Prime minister of 2004: {primeMinisterData[2004]}");
 
         // Add the current prime minister in the dictionary.
-        PrimeMinisterData.Add(2024, "Narendra Modi");
+        primeMinisterData.Add(2024, "Narendra Modi");
 
         // Sort the dictionary by year.
-        var sortedDictionary = PrimeMinisterData.OrderBy(x=>x.Key);
+        var sortedDictionary = primeMinisterData.OrderBy(x=>x.Key);
         foreach (var kvp in sortedDictionary)
         {
             Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
